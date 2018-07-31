c4c46e50ddcadef69095335780722e436d55bfed
cjyueh <cjyueh@gmail.com>
2018-07-31 12:01:51 -0700

    replace images with high-res, add page scrolling

--- main.js
+++ main.js
@@ -1,23 +1,85 @@
-var slideLeft = new Menu({
-  wrapper: '#wrapper',
-  type: 'slide-left',
-  menuOpenerClass: '.hamburger',
-  maskId: '#mask'
-});
-var slideLeftBtn = document.querySelector('#hamburger');
-
-slideLeftBtn.addEventListener('mouseover', function(e) {
+$('.link-home').click(function(e) {
   e.preventDefault();
-  slideLeft.open();
-});
-
-var mobile = (navigator.userAgent.match(/(iPad|iPhone|Android)/g) ? true : false);
-
-$('#top-arrow').click(function() {
-  if (mobile) {
-    $("html, body", parent.document).animate({ scrollTop: 0 }, 200);
+  console.log('home clicked');
+  if ($('.home').hasClass('show')) {
+    $('.home').removeClass('show');
+    $('.yoga').removeClass('position-2');
+    $('.ux').removeClass('position-4');
+    $('.yoga').removeClass('position-1');
+    $('.yoga').removeClass('show');
+    $('.yoga').removeClass('position-4');
+    $('.yoga').removeClass('position-5');
+    $('.home').removeClass('position-1');
+    $('.home').removeClass('position-2');
+    $('.home').removeClass('position-4');
+    $('.home').removeClass('position-5');
+    $('.ux').removeClass('position-1');
+    $('.ux').removeClass('position-2');
+    $('.ux').removeClass('show');
+    $('.ux').removeClass('position-5');
   } else {
-    $("html, body").animate({ scrollTop: 0 }, 500);
+    $('.yoga').addClass('position-2');
+    $('.home').addClass('show');
+    $('.ux').addClass('position-4');
+    $('.yoga').removeClass('position-1');
+    $('.yoga').removeClass('show');
+    $('.yoga').removeClass('position-4');
+    $('.yoga').removeClass('position-5');
+    $('.home').removeClass('position-1');
+    $('.home').removeClass('position-2');
+    $('.home').removeClass('position-4');
+    $('.home').removeClass('position-5');
+    $('.ux').removeClass('position-1');
+    $('.ux').removeClass('position-2');
+    $('.ux').removeClass('show');
+    $('.ux').removeClass('position-5');
   }
 });
 
+$('.link-ux').click(function(e) {
+  e.preventDefault();
+  console.log('ux clicked');
+  if ($('.ux').hasClass('show')) {
+    $('.ux').removeClass('show');
+  } else {
+    $('.yoga').addClass('position-1');
+    $('.home').addClass('position-2');
+    $('.ux').addClass('show');
+    $('.yoga').removeClass('position-2');
+    $('.yoga').removeClass('show');
+    $('.yoga').removeClass('position-4');
+    $('.yoga').removeClass('position-5');
+    $('.home').removeClass('position-1');
+    $('.home').removeClass('show');
+    $('.home').removeClass('position-4');
+    $('.home').removeClass('position-5');
+    $('.ux').removeClass('position-1');
+    $('.ux').removeClass('position-2');
+    $('.ux').removeClass('position-4');
+    $('.ux').removeClass('position-5');
+  }
+});
+
+$('.link-yoga').click(function(e) {
+  e.preventDefault();
+  console.log('yoga clicked');
+  if ($('.yoga').hasClass('show')) {
+    $('.yoga').removeClass('show');
+  } else {
+    $('.yoga').addClass('show');
+    $('.home').addClass('position-4');
+    $('.ux').addClass('position-5');
+    $('.yoga').removeClass('position-1');
+    $('.yoga').removeClass('position-2');
+    $('.yoga').removeClass('position-4');
+    $('.yoga').removeClass('position-5');
+    $('.home').removeClass('position-1');
+    $('.home').removeClass('position-2');
+    $('.home').removeClass('show');
+    $('.home').removeClass('position-5');
+    $('.ux').removeClass('position-1');
+    $('.ux').removeClass('position-2');
+    $('.ux').removeClass('position-4');
+    $('.ux').removeClass('show');
+  }
+});
